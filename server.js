fb10963cb5beaba194c81b85c889e185932d7ab5
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-13 14:42:11 -0500

    get and listen added

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cor')
+const { request } = require('http')
 const PORT = 8000
 
 app.use(cors())
+
+const aliens = {}
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:alienName', (req, res)=>{
+    const aliensName = req.params.alienName.toLowerCase()
+    if(aliens[aliensName]){
+        res.json(aliens[aliensName])
+    } else {
+        res.json(aliens['humans'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log('server is running')
+})
